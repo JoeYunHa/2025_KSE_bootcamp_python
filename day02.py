5ae51606b8893427733210d498bcb81bf9bca764
JoeYunHa <12201806@inha.edu>
2025-02-11 10:56:38 +0900

    Add day02.py

---
+++ day02.py
@@ -0,0 +1,15 @@
+n = int(input("Input number : "))
+is_prime = True
+
+if n >= 2 :
+    for i in range(2, n):
+        if n % i == 0:
+            is_prime = False
+            break
+else:
+    is_prime = False
+
+if is_prime:
+    print(f"{n} is prime number")
+else:
+    print(f"{n} is NOT prime number!")
