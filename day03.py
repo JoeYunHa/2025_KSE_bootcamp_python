19be528594a71ee616b6326a4e4b14edadb5d747
JoeYunHa <12201806@inha.edu>
2025-02-12 17:24:58 +0900

    Modify day03.py

--- day03.py
+++ day03.py
@@ -3,12 +3,17 @@ import random
 drinks = ["위스키", "와인", "소주", "고량주","사케","위스키"]
 foods = ["초콜릿","치즈","삼겹살","양꼬치","광어회","낙곱새"]
 
+
 def print_menu(index):
     index -= 1
     print(f'{drinks[index]}에 어울리는 안주는 {foods[index]} 입니다')
 
 while True:
-    menu = int(input(f'다음 술중에 고르세요.\n1) {drinks[0]}   2) {drinks[1]}   3) {drinks[2]}   4) {drinks[3]}   5) {drinks[4]}   6) 아무거나   7) 종료 : '))
+    menu_lists = '다음 술중에 고르세요.'
+    for i in range(len(drinks)):
+        menu_lists += f'\t{i + 1}) {drinks[i]}'
+    menu_lists += f'\t{len(drinks)}) 아무거나\t{len(drinks)+1}) 종료\n'
+    menu = int(input(menu_lists))
     if menu <= (len(drinks) - 1):
         print_menu(menu)
     elif menu == len(drinks):
