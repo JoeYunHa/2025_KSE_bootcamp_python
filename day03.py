62a85323dc38252763603415d1a3235f0b6491b7
JoeYunHa <12201806@inha.edu>
2025-02-12 17:11:50 +0900

    Modify day03.py

--- day03.py
+++ day03.py
@@ -11,9 +11,9 @@ while True:
     menu = int(input(f'다음 술중에 고르세요.\n1) {drinks[0]}   2) {drinks[1]}   3) {drinks[2]}   4) {drinks[3]}   5) {drinks[4]}   6) 아무거나   7) 종료 : '))
     if menu <= (len(drinks) - 1):
         print_menu(menu)
-    elif menu == 6:
+    elif menu == len(drinks):
         random_index = random.randint(0,len(drinks) - 1)
         print_menu(random_index)
-    elif menu == 7:
+    elif menu == len(drinks) + 1:
         print(f'다음에 또 오세요')
         break
