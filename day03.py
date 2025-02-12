033ca9346627ac093cefe0d1d22c524b6c2b99db
JoeYunHa <12201806@inha.edu>
2025-02-12 17:01:45 +0900

    Modify day03.py

--- day03.py
+++ day03.py
@@ -3,21 +3,17 @@ import random
 drinks = ["위스키", "와인", "소주", "고량주","사케","위스키"]
 foods = ["초콜릿","치즈","삼겹살","양꼬치","광어회","낙곱새"]
 
+def print_menu(index):
+    index -= 1
+    print(f'{drinks[index]}에 어울리는 안주는 {foods[index]} 입니다')
+
 while True:
-    menu = input(f'다음 술중에 고르세요.\n1) {drinks[0]}   2) {drinks[1]}   3) {drinks[2]}   4) {drinks[3]}   5) {drinks[4]}   6) 아무거나   7) 종료 : ')
-    if menu == '1':
-        print(f'{drinks[0]}에 어울리는 안주는 {foods[0]} 입니다')
-    elif menu == '2':
-        print(f'{drinks[1]}에 어울리는 안주는 {foods[1]} 입니다')
-    elif menu == '3':
-        print(f'{drinks[2]}에 어울리는 안주는 {foods[2]} 입니다')
-    elif menu == '4':
-        print(f'{drinks[3]}에 어울리는 안주는 {foods[3]} 입니다')
-    elif menu == '5':
-        print(f'{drinks[4]}에 어울리는 안주는 {foods[4]} 입니다')
-    elif menu == '6':
-        random_index = random.randint(0,6)
-        print(f'{drinks[random_index]}에 어울리는 안주는 {foods[random_index]} 입니다')
-    elif menu == '7':
+    menu = int(input(f'다음 술중에 고르세요.\n1) {drinks[0]}   2) {drinks[1]}   3) {drinks[2]}   4) {drinks[3]}   5) {drinks[4]}   6) 아무거나   7) 종료 : '))
+    if menu <= (len(drinks) - 1):
+        print_menu(menu)
+    elif menu == 6:
+        random_index = random.randint(0,len(drinks) - 1)
+        print_menu(random_index)
+    elif menu == 7:
         print(f'다음에 또 오세요')
         break
