c244c59c363578d8c9292ee9bc07104a36c1dca0
JoeYunHa <12201806@inha.edu>
2025-02-14 10:52:52 +0900

    Modify day05.py fibonacci

--- day05.py
+++ day05.py
@@ -1,11 +1,49 @@
 # recursion
+# import time
+#
+# def time_decorator(func):
+#     def wrapper(*args):
+#         s = time.time()
+#         r = func(*args)
+#         e = time.time()
+#         print(f"{e - s} s")
+#         return r
+#     return wrapper
 
-def factorial_recursion(n):
-    if n == 1:
-        return n
+# @time_decorator
+def fibonacci_recursion(n)->int:
+    """
+    Return fibonacci by recursion
+    :param n: integer number
+    :return: fibonacci(n)
+    """
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
     else:
-        return n * factorial_recursion(n-1)
+        return fibonacci_recursion(n-2) + fibonacci_recursion(n-1)
+
+# @time_decorator
+def fibonacci_loop(n)->int:
+    """
+    Return fibonacci by loop
+    :param n: integer number
+    :return: fibonacci(n)
+    """
+    res = 0
+    a = 0
+    b = 1
+    i = 1
+    while i < n :
+        res = a+b
+        a = b
+        b = res
+        i += 1
+    return res
 
 number = int(input("number : "))
-print(factorial_recursion(number))
+print(fibonacci_recursion(number))
+print(fibonacci_loop(number))
 
+# time_decorator -> recursion마다 실행됨
