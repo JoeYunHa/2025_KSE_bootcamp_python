8f4ca31abdacfc14218e058add45c28487160bd8
JoeYunHa <12201806@inha.edu>
2025-02-14 17:09:39 +0900

    Modify day05.day add my_abs

--- day05.py
+++ day05.py
@@ -1,21 +1,11 @@
-# recursion
+# class 간의 관계
+# is - a
+# inheritance
+# Association = composition, aggregation
+# part - of => composition
+# has - a => aggregation
 
-def count_down_recursion(n):
-    if n == 0:
-        print("boom!")
-        return
-    else:
-        print(f"{n}!")
-        return count_down_recursion(n-1)
-
-def count_down_loop(n):
-    while n > 0:
-        print(f"{n}!")
-        n -= 1
-    print(f"BOOM!")
-    return
-
-
-num = int(input())
-count_down_recursion(num)
-count_down_loop(num)
+def my_abs(n)->int:
+    if n < 0:
+        return -n
+    return n
